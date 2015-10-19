360e0aed71aece4f84895675cfc6eceace2d7e36
GT Deng <gtdeng@users.noreply.github.com>
2015-10-18 20:24:37 -0700

    update ip address

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ var server = http.createServer(app);
 
 middleware(app, express);
 
-server.listen(80, '104.236.172.91', function() {
-  console.log('Listening on 104.236.172.91:' + server.address().port);
+server.listen(80, '104.131.139.49', function() {
+  console.log('Listening on 104.131.139.49:' + server.address().port);
 });
 
 module.exports = app;
